adeca9e0045025805f6e1d1aa97a2004bcdb334c
Serik <serj_navii@mail.ru>
2021-04-09 08:09:36 +0600

    solve task 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,22 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  if (!domains.length) return {};
+  let doms = domains.map((dom) => dom.split('.').reverse().map((el) => `.${el}`));
+  const reDoms = doms.map((el) => el.join(''));
+  doms = doms.map((dom) => dom.map((el, index) => dom.slice(0, index + 1).join('')));
+  const obj = {};
+  for (let i = 0; i < reDoms.length; i++) {
+    for (let j = 0; j < doms[i].length; j++) {
+      if (Object.keys(obj).includes(doms[i][j])) {
+        obj[doms[i][j]] += 1;
+      } else {
+        obj[doms[i][j]] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
